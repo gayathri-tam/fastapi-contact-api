895338b92976405c0548204e77485f41aa82945a
Gayathri <gayathri18025@gmail.com>
2026-09-22 19:01:35 +0530

    main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=False
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
